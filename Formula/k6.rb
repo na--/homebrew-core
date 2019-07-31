0d28c2a69f7716f078d744bbdacbb11c0f709439
Nedyalko Andreev <n@andreev.sh>
2019-07-31 15:36:04 +0300

    k6 0.25.0

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -2,15 +2,8 @@ class K6 < Formula
   desc "Modern load testing tool, using Go and JavaScript"
   homepage "https://k6.io"
   url "https://github.com/loadimpact/k6.git",
-    :tag      => "v0.24.0",
-    :revision => "f597e5f66d22f697a386752ea0f4b0361765ab34"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "7bf3b04410bacde97e906860addced0403d96a4c0fcab8efcec811c4a0bf9da3" => :mojave
-    sha256 "0a59d836290c7b499dfa5718dcbf05559f6fdc9b287a737bbfecf0bdb6dc3c6e" => :high_sierra
-    sha256 "3c32a857ca8b388bc4715bf73a3f661a500d25795c6b213012beb8c6bab657f4" => :sierra
-  end
+    :tag      => "v0.25.0",
+    :revision => "c53607db0ef32be9535d1b516c90275fb37a54cf"
 
   depends_on "dep" => :build
   depends_on "go" => :build
